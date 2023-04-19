4d517b9e1bdc928643e49b0b6448f65818c6f8bf
Adrian Molina <adrmr26@gmail.com>
2023-04-18 21:45:33 -0600

    Paneles

--- Main.c
+++ Main.c
@@ -1,10 +1,19 @@
+struct Panel {
+	int Bateria;
+};
+
+
+
 struct Requiere {
 	char Area[10];
 	int Tiempo;
 	int Bateria;
 };
 
+
+
 int main(){
+	struct Panel P = {100};
 	struct Requiere s1 ={"Gym",12,11};
 	
 	printf("%s %d %d", s1.Area, s1.Bateria, s1.Tiempo);
