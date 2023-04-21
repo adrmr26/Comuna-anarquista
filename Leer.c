5b791aff29d4153119e846edf7261f3980c83a12
Adrian Molina <66936455+adrmr26@users.noreply.github.com>
2023-04-21 00:09:11 -0600

    Update Leer.c
    Sirve por lo menos

--- Leer.c
+++ Leer.c
@@ -1,24 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <sys/stat.h>
 
-int main()
-{
-	FILE *archivo;
-	char carater;
+#define BUFFER_SIZE 1024
 
-	archivo =fopen("Mantener huerto.txt","r");
-	if (archivo==NULL){
-		printf("Error en la apertura del archivo\n");
-	}else{
-		printf("Si sirvio\n\n");
-		while(feof(archivo)==0){
-			carater=fgetc(archivo);
-			printf("%c",carater);
+struct Requiere {
+    char area[BUFFER_SIZE];
+    int bateria;
+    int tiempo;
+};
 
-		}
-	}
+struct Trabaja {
+    int bateria;
+    int tiempo;
+};
 
-	fclose(archivo);
-	getch();
-	return 0;
+int main() {
+    char* path = "C:\\Users\\Adrmr\\Documents\\UNIVERSIDAD\\Operativos\\Tareas de la comuna\\";
+    DIR* directory = opendir(path);
+    struct dirent* file;
+
+    // Listas para almacenar los datos
+    struct Requiere requieres[100];
+    int num_requiere = 0;
+    struct Trabaja trabajas[100];
+    int num_trabaja = 0;
+
+    if (directory != NULL) {
+        while ((file = readdir(directory)) != NULL) {
+            if (strcmp(file->d_name, ".") != 0 && strcmp(file->d_name, "..") != 0) {
+                char filepath[BUFFER_SIZE];
+                snprintf(filepath, BUFFER_SIZE, "%s%s", path, file->d_name);
+
+                // Leer el contenido del archivo
+                FILE* fp = fopen(filepath, "r");
+                char buffer[BUFFER_SIZE];
+                while (fgets(buffer, BUFFER_SIZE, fp)) {
+                    char* token = strtok(buffer, " ");
+                    if (token != NULL) {
+                        if (strcmp(token, "Requiere") == 0) {
+                            // Extraer los datos y agregar a la lista de requieres
+                            struct Requiere req;
+                            strcpy(req.area, strtok(NULL, " "));
+                            req.bateria = atoi(strtok(NULL, " "));
+                            req.tiempo = atoi(strtok(NULL, " "));
+                            requieres[num_requiere++] = req;
+                        } else if (strcmp(token, "Trabaja") == 0) {
+                            // Extraer los datos y agregar a la lista de trabajas
+                            struct Trabaja tra;
+                            tra.bateria = atoi(strtok(NULL, " "));
+                            tra.tiempo = atoi(strtok(NULL, " "));
+                            trabajas[num_trabaja++] = tra;
+                        }
+                    }
+                }
+                fclose(fp);
+            }
+        }
+        closedir(directory);
+
+        // Imprimir los datos
+        printf("Requiere:\n");
+        for (int i = 0; i < num_requiere; i++) {
+            printf("%s %d %d\n", requieres[i].area, requieres[i].bateria, requieres[i].tiempo);
+        }
+        printf("\nTrabaja:\n");
+        for (int i = 0; i < num_trabaja; i++) {
+            printf("%d %d\n", trabajas[i].bateria, trabajas[i].tiempo);
+        }
+    } else {
+        printf("Error al abrir el directorio\n");
+    }
+
+    return 0;
 }
